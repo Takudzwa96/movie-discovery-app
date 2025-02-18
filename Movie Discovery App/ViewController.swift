d8a562bc8ae70de9b9d663a7991abcf95d6b3ce7
Takudzwa Raisi <takudzwa.raisi@ayoba.me>
2025-02-18 14:17:13 +0200

    Initial Commit

---
+++ Movie Discovery App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie Discovery App
+//
+//  Created by Takudzwa Raisi on 2025/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Movie Discovery AppTests/Movie_Discovery_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_Discovery_AppTests.swift
+//  Movie Discovery AppTests
+//
+//  Created by Takudzwa Raisi on 2025/02/18.
+//
+
+import Testing
+@testable import Movie_Discovery_App
+
+struct Movie_Discovery_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
